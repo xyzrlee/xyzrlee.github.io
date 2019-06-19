40b30de943b7a5deb147cbd4c16d017b6601cafd
Ricky Li <cnrickylee@gmail.com>
2019-06-19 16:20:47 +0800

    update script

--- script/travis_git_push.sh
+++ script/travis_git_push.sh
@@ -5,7 +5,7 @@ script_path=$(cd `dirname $0`; pwd)
 pushd ${script_path}
 cd ..
 git add surge/ad.list
-git diff --cached --exit-code
+git diff --cached --exit-code >/dev/null
 if [ $? -ne 0 ]; then
     echo "change detected"
 fi
