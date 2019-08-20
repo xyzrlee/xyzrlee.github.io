b8537b5e575126fd6672d2ada63ad2ed5a8d39b7
Ricky Li <cnrickylee@gmail.com>
2019-08-20 16:56:50 +0800

    add

---
+++ script/dnscrypt-proxy-forwarding.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+script_path=$(cd `dirname $0`; pwd)
+pushd ${script_path}/..
+
+DIR="$(mktemp -d)"
+git clone --depth=1 https://github.com/felixonmars/dnsmasq-china-list.git ${DIR}
+pushd ${DIR}
+make SERVER=223.5.5.5,223.6.6.6,119.29.29.29 dnscrypt-proxy
+popd
+mkdir -p dnscrypt-proxy
+cp ${DIR}/dnscrypt-proxy-forwarding-rules.txt dnscrypt-proxy/forwarding-rules.txt
+
+popd
