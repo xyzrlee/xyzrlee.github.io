e43926c9760388cc39ccd6fd823a6babb04dd97a
xyzrlee <cnrickylee@gmail.com>
2019-08-23 15:38:56 +0800

    fix

--- script/privoxy-gfwlist.sh
+++ script/privoxy-gfwlist.sh
@@ -4,8 +4,10 @@ script_path=$(cd `dirname $0`; pwd)
 pushd ${script_path}/..
 
 DIR="$(mktemp -d)"
-mkdir -p ../privoxy
 wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O ${DIR}/gfwlist.txt
-gfwlist2privoxy -i ${DIR}/gfwlist.txt -f ../privoxy/gfwlist.action -p 127.0.0.1:1080 -t socks5
+gfwlist2privoxy -i ${DIR}/gfwlist.txt -f ${DIR}/gfwlist.action -p 127.0.0.1:1080 -t socks5
+
+mkdir -p privoxy
+cp ${DIR}/gfwlist.action privoxy/
 
 popd
